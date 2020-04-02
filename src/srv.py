526cb12ce06a1e1986ba3759021211eec55343be
Jan Adam <siggi2017@freenet.de>
2020-04-02 20:51:26 +0200

    dynamic image filenames

--- src/srv.py
+++ src/srv.py
@@ -1,15 +1,33 @@
 from flask import Flask, escape, request, render_template
+import os
+import sys
 
 app = Flask(__name__)
+
 
 
 #def hello():
 #    name = request.args.get("name", "World")
 #    return f'Hello, {escape(name)}!'
+scripdir = os.path.dirname(sys.argv[0])
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    staticpath = os.path.join(scripdir, "static")
+    files = os.listdir(staticpath)
+    inputfilename = None
+    for filename in files:
+        if "input" in filename:
+            inputfilename = filename
+            print( "input filename: ", inputfilename)
+            break
+    for filename in files:
+        if "output" in filename:
+            outputfilename = filename
+            print( "output filename: ", outputfilename)
+            break
+
+    return render_template('index.html', inputfilename=inputfilename, outputfilename=outputfilename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
